bed2e19b85e764798ea7a9c4fd74c007a0b95fd2
JayeAnalyst <jaiyeobaoluwaseun04@gmail.com>
2024-08-07 07:20:04 -0500

    Top 50 Business Analyst Paying Jobs

---
+++ project_sql/1_Top_paying_jobs.sql
@@ -0,0 +1,29 @@
+/*
+Identify the top 50 highest-paying Business Analyst roles that are available remotely.
+Focuses on job postings with specified salaries (Remove NULLs)
+Why? Aims to highlight the top-paying opportunities for Business Analysts, offering insights into employment options and location flexibility.
+*/
+
+SELECT
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date,
+    name AS company_name
+FROM
+    job_postings_fact
+LEFT JOIN company_dim
+    ON job_postings_fact.company_id = company_dim.company_id
+WHERE 
+    job_title_short = 'Business Analyst' AND 
+    job_location = 'Anywhere' AND 
+    salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
+LIMIT 50;
+
+
+
+
